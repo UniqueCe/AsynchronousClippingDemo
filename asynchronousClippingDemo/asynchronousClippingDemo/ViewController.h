ca88a59b05fcc1c22d77bab59b4b32f5effb9a52
UniqueCe <15011529145@163.com>
2016-11-01 13:54:52 +0800

    创建项目

---
+++ asynchronousClippingDemo/asynchronousClippingDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  asynchronousClippingDemo
+//
+//  Created by 刘培策 on 16/11/1.
+//  Copyright © 2016年 UniqueCe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
