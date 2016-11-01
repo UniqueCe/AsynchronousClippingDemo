b5f2af6c7f90dcddb1d17d09c77d38f2440fa716
UniqueCe <15011529145@163.com>
2016-11-01 13:59:28 +0800

    创建分类

---
+++ asynchronousClippingDemo/asynchronousClippingDemo/Extensin/UIImage+Extension.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+Extension.h
+//  asynchronousClippingDemo
+//
+//  Created by 刘培策 on 16/11/1.
+//  Copyright © 2016年 UniqueCe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Extension)
+
+@end
---
+++ asynchronousClippingDemo/asynchronousClippingDemo/Extensin/UIImage+Extension.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+Extension.m
+//  asynchronousClippingDemo
+//
+//  Created by 刘培策 on 16/11/1.
+//  Copyright © 2016年 UniqueCe. All rights reserved.
+//
+
+#import "UIImage+Extension.h"
+
+@implementation UIImage (Extension)
+
+@end
